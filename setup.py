cd5d796dd808cd9c795ed6f05053e56732dfdd63
nymwa <nymwa0@gmail.com>
2021-09-03 22:44:09 +0900

    ver 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name = 'ilonimi',
-        version = '0.1.0',
+        version = '1.0.0',
         packages = setuptools.find_packages(),
         entry_points = {
             'console_scripts':[
