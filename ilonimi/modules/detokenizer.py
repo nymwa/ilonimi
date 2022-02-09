0992edfa9fb3dee1549c72dc8154ab46ff4b81a1
nymwa <nymwa0@gmail.com>
2022-02-10 08:13:50 +0900

    quot

--- ilonimi/modules/detokenizer.py
+++ ilonimi/modules/detokenizer.py
@@ -3,13 +3,15 @@ import re
 class Detokenizer:
     def __init__(self):
         self.punct_pattern = re.compile(r' ([!,.:?])')
-        self.quot_pattern = re.compile(r'" ([^"]*) "')
+        self.quot_pattern1 = re.compile(r'" ([^"]*) "')
+        self.quot_pattern2 = re.compile(r'\' ([^\']*) \'')
 
     def __call__(self, x):
         if type(x) == list:
             x = ' '.join(x)
         x = x.strip()
         x = self.punct_pattern.sub('\\1', x)
-        x = self.quot_pattern.sub('"\\1"', x)
+        x = self.quot_pattern1.sub('"\\1"', x)
+        x = self.quot_pattern2.sub('"\\1"', x)
         return x
 
