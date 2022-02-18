6244803ede706faa572382b5ac3ce645c6d268d9
nymwa <nymwa0@gmail.com>
2022-02-18 15:48:25 +0900

    add paren

--- ilonimi/modules/detokenizer.py
+++ ilonimi/modules/detokenizer.py
@@ -5,6 +5,7 @@ class Detokenizer:
         self.punct_pattern = re.compile(r' ([!,.:?])')
         self.quot_pattern1 = re.compile(r'" ([^"]*) "')
         self.quot_pattern2 = re.compile(r'\' ([^\']*) \'')
+        self.paren_pattern = re.compile(r'\( ([^)]*) \)')
 
     def __call__(self, x):
         if type(x) == list:
@@ -12,6 +13,7 @@ class Detokenizer:
         x = x.strip()
         x = self.punct_pattern.sub('\\1', x)
         x = self.quot_pattern1.sub('"\\1"', x)
-        x = self.quot_pattern2.sub('"\\1"', x)
+        x = self.quot_pattern2.sub('\'\\1\'', x)
+        x = self.paren_pattern.sub('(\\1)', x)
         return x
 
