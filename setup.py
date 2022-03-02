c7ff64bd0e95498a999d517ae1c9b6bf75d8dc37
nymwa <nymwa0@gmail.com>
2022-03-02 10:01:33 +0900

    add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name = 'ilonimi',
-        version = '1.1.0',
+        version = '1.2.0',
         packages = setuptools.find_packages(),
         entry_points = {'console_scripts':['nimi = ilonimi.main:main',]})
 
