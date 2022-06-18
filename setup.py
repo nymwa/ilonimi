b345966a9031fe6fe50e344786dd556e89d4c6c1
nymwa <nymwa0@gmail.com>
2022-06-18 16:47:42 +0900

    add

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 import setuptools
 
+
+with open('README.md') as f:
+    long_discription = f.read()
+
+
 setuptools.setup(
         name = 'ilonimi',
         version = '1.3.0',
+        author = 'nymwa',
+        author_email = 'nymwa0@gmail.com',
+        description = 'Toki Pona Tokenizer / Detokenizer / Kanaizer',
+        long_discription = long_discription,
+        long_discription_content_type = 'text/markdown',
+        url = 'https://github.com/nymwa/ilonimi',
+        classifiers = [
+            'Programming Language :: Python :: 3.8',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent'],
         packages = setuptools.find_packages(),
-        entry_points = {'console_scripts':['nimi = ilonimi.main:main',]})
+        entry_points = {'console_scripts':['nimi = ilonimi.main:main']})
 
